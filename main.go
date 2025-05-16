305a3e8ab9489299e18302f3def06b43e513802f
Somnathumapathi <somnathumapathi7@gmail.com>
2025-05-16 21:17:15 +0530

    Init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// var db = make(map[string]string)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080")
+}
